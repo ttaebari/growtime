2630354cc87f028544ede6a0b4cd85a548873f38
ttaebari <taehotuna@naver.com>
2026-04-24 10:24:37 +0900

    feat : GitHub API 호출 안정화

--- src/main/kotlin/com/board/growtime/GrowtimeApplication.kt
+++ src/main/kotlin/com/board/growtime/GrowtimeApplication.kt
@@ -2,18 +2,22 @@ package com.board.growtime
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.web.client.RestTemplate
+import java.time.Duration
 
 @SpringBootApplication
 @EnableJpaAuditing
 class GrowtimeApplication {
 
     @Bean
-    fun restTemplate(): RestTemplate {
-        return RestTemplate()
+    fun restTemplate(builder: RestTemplateBuilder): RestTemplate {
+        return builder
+            .connectTimeout(Duration.ofSeconds(3))
+            .readTimeout(Duration.ofSeconds(5))
+            .build()
     }
 }
 
